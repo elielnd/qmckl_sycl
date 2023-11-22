b72588fa2a1931d41c38960f6954892c10682dd8
marcloic <marcloic81@gmail.com>
2023-11-22 16:54:12 +0000

    Done with qmckl_memory_omp_sycl.cpp

---
+++ src/qmckl_memory_omp_sycl.cpp
@@ -0,0 +1,304 @@
+#include "../include/qmckl_memory_sycl.h"
+#include <assert.h>
+
+// This file contains functions prototypes for context memory management
+// functions (on device only, we expect most if not all of the context
+// memory to be allocated on device in most cases)
+// (OpenMP implementations)
+
+//**********
+// ALLOCS / FREES
+//**********
+
+void *qmckl_malloc_device(sycl::queue &queue, qmckl_context_device context, size_t size)
+{
+	assert(qmckl_context_check_device(context) != QMCKL_NULL_CONTEXT_DEVICE);
+
+	qmckl_context_struct_device *const ctx =
+		reinterpret_cast<qmckl_context_struct_device *>(context);
+	int device_id = qmckl_get_device_id(context);
+
+	// Allocate memory and zero it using USM
+	void *pointer = sycl::malloc_device(size, queue);
+	if (pointer == nullptr)
+	{
+		return nullptr;
+	}
+
+	// TODO
+	// Memset to 0 of size info.size
+	// sycl::memset(pointer, 0, size, queue);
+
+	qmckl_lock_device(context);
+	{
+		// If qmckl_memory_struct is full, reallocate a larger one
+		if (ctx->memory_device.n_allocated == ctx->memory_device.array_size)
+		{
+			const size_t old_size = ctx->memory_device.array_size;
+			qmckl_memory_info_struct_device *new_array = static_cast<qmckl_memory_info_struct_device *>(
+				sycl::malloc_device(2L * old_size * sizeof(qmckl_memory_info_struct_device), queue));
+
+			if (new_array == nullptr)
+			{
+				qmckl_unlock_device(context);
+				sycl::free(pointer, queue);
+				return nullptr;
+			}
+
+			sycl::memset(&(new_array[old_size]), 0, old_size * sizeof(qmckl_memory_info_struct_device), queue);
+			sycl::memcpy(new_array, ctx->memory_device.element,
+						 old_size * sizeof(qmckl_memory_info_struct_device), queue)
+				.wait_and_throw();
+
+			sycl::free(ctx->memory_device.element, queue);
+			ctx->memory_device.element = new_array;
+			ctx->memory_device.array_size = 2L * old_size;
+		}
+
+		// Find first NULL entry
+		size_t pos = (size_t)0;
+		while (pos < ctx->memory_device.array_size &&
+			   ctx->memory_device.element[pos].size > (size_t)0)
+		{
+			pos += (size_t)1;
+		}
+		assert(ctx->memory_device.element[pos].size == (size_t)0);
+
+		// Copy info at the new location
+		ctx->memory_device.element[pos].size = size;
+		ctx->memory_device.element[pos].pointer = pointer;
+		ctx->memory_device.n_allocated += (size_t)1;
+	}
+	qmckl_unlock_device(context);
+
+	return pointer;
+}
+
+qmckl_exit_code_device qmckl_free_device(qmckl_context_device context, void *const ptr)
+{
+	if (qmckl_context_check_device(context) == QMCKL_NULL_CONTEXT_DEVICE)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_CONTEXT_DEVICE,
+									 "qmckl_free_device", nullptr);
+	}
+
+	if (ptr == nullptr)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_ARG_2_DEVICE,
+									 "qmckl_free_device", "NULL pointer");
+	}
+
+	qmckl_context_struct_device *const ctx =
+		(qmckl_context_struct_device *)context;
+	int device_id = qmckl_get_device_id(context);
+
+	qmckl_lock_device(context);
+	{
+		/* Find pointer in array of saved pointers */
+		size_t pos = (size_t)0;
+		while (pos < ctx->memory_device.array_size &&
+			   ctx->memory_device.element[pos].pointer != ptr)
+		{
+			pos += (size_t)1;
+		}
+
+		if (pos >= ctx->memory_device.array_size)
+		{
+			/* Not found */
+			qmckl_unlock_device(context);
+			return qmckl_failwith_device(context, QMCKL_FAILURE_DEVICE,
+										 "qmckl_free_device",
+										 "Pointer not found in context");
+		}
+
+		sycl::free(ptr, ctx->memory_device.qct);
+
+		memset(&(ctx->memory_device.element[pos]), 0,
+			   sizeof(qmckl_memory_info_struct_device));
+		ctx->memory_device.n_allocated -= (size_t)1;
+	}
+	qmckl_unlock_device(context);
+
+	return QMCKL_SUCCESS_DEVICE;
+}
+
+//**********
+// MEMCPYS
+//**********
+
+qmckl_exit_code_device qmckl_memcpy_H2D(qmckl_context_device context,
+										void *const dest, void *const src,
+										size_t size)
+{
+	if (qmckl_context_check_device(context) == QMCKL_NULL_CONTEXT_DEVICE)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_CONTEXT_DEVICE,
+									 "qmckl_memcpy_H2D", nullptr);
+	}
+
+	if (dest == nullptr)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_ARG_2_DEVICE,
+									 "qmckl_memcpu_H2D", "NULL dest pointer");
+	}
+
+	if (src == nullptr)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_ARG_3_DEVICE,
+									 "qmckl_memcpu_H2D", "NULL src pointer");
+	}
+
+	qmckl_lock_device(context);
+	{
+		try
+		{
+			// Get the SYCL queue associated with the context
+			queue q(context.get_sycl_queue());
+
+			// Use USM for memory management
+			// Memory allocation and data copy to device
+			void *dest_device = malloc_device(size, q);
+			void *src_device = malloc_device(size, q);
+			memcpy(dest_device, dest, size);
+			memcpy(src_device, src, size);
+
+			// Perform data transfer using SYCL command group
+			q.submit([&](handler &h)
+					 { h.parallel_for(range<1>(size), [=](id<1> i)
+									  { reinterpret_cast<char *>(dest_device)[i] =
+											reinterpret_cast<char *>(src_device)[i]; }); });
+
+			// Free allocated device memory
+			free(dest_device, q);
+			free(src_device, q);
+		}
+		catch (sycl::exception const &e)
+		{
+			// Handle exceptions, if any
+			std::cerr << "SYCL Exception: " << e.what() << std::endl;
+			return qmckl_failwith_device(context, QMCKL_FAILURE_DEVICE,
+										 "qmckl_memcpy_H2D", "SYCL exception");
+		}
+	}
+	qmckl_unlock_device(context);
+
+	return QMCKL_SUCCESS_DEVICE;
+}
+
+qmckl_exit_code_device qmckl_memcpy_D2H(qmckl_context_device context,
+										void *const dest, void *const src,
+										size_t size)
+{
+	if (qmckl_context_check_device(context) == QMCKL_NULL_CONTEXT_DEVICE)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_CONTEXT_DEVICE,
+									 "qmckl_memcpy_D2H", nullptr);
+	}
+
+	if (dest == nullptr)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_ARG_2_DEVICE,
+									 "qmckl_memcpu_D2H", "NULL dest pointer");
+	}
+
+	if (src == nullptr)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_ARG_3_DEVICE,
+									 "qmckl_memcpu_D2H", "NULL src pointer");
+	}
+
+	qmckl_lock_device(context);
+	{
+		try
+		{
+			// Get the SYCL queue associated with the context
+			queue q(context.get_sycl_queue());
+
+			// Use USM for memory management
+			// Memory allocation and data copy to device
+			void *dest_device = malloc_device(size, q);
+			void *src_device = malloc_device(size, q);
+			memcpy(dest_device, dest, size);
+			memcpy(src_device, src, size);
+
+			// Perform data transfer using SYCL command group
+			q.submit([&](handler &h)
+					 { h.parallel_for(range<1>(size), [=](id<1> i)
+									  { reinterpret_cast<char *>(dest_device)[i] =
+											reinterpret_cast<char *>(src_device)[i]; }); });
+
+			// Free allocated device memory
+			free(dest_device, q);
+			free(src_device, q);
+		}
+		catch (sycl::exception const &e)
+		{
+			// Handle exceptions, if any
+			std::cerr << "SYCL Exception: " << e.what() << std::endl;
+			return qmckl_failwith_device(context, QMCKL_FAILURE_DEVICE,
+										 "qmckl_memcpy_D2H", "SYCL exception");
+		}
+	}
+	qmckl_unlock_device(context);
+
+	return QMCKL_SUCCESS_DEVICE;
+}
+
+qmckl_exit_code_device qmckl_memcpy_D2D(qmckl_context_device context,
+										void *const dest, void *const src,
+										size_t size)
+{
+	if (qmckl_context_check_device(context) == QMCKL_NULL_CONTEXT_DEVICE)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_CONTEXT_DEVICE,
+									 "qmckl_memcpy_D2D", nullptr);
+	}
+
+	if (dest == nullptr)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_ARG_2_DEVICE,
+									 "qmckl_memcpu_D2D", "NULL dest pointer");
+	}
+
+	if (src == nullptr)
+	{
+		return qmckl_failwith_device(context, QMCKL_INVALID_ARG_3_DEVICE,
+									 "qmckl_memcpu_D2D", "NULL src pointer");
+	}
+
+	qmckl_lock_device(context);
+	{
+		try
+		{
+			// Get the SYCL queue associated with the context
+			queue q(context.get_sycl_queue());
+
+			// Use USM for memory management
+			// Memory allocation and data copy to device
+			void *dest_device = malloc_device(size, q);
+			void *src_device = malloc_device(size, q);
+			memcpy(dest_device, dest, size);
+			memcpy(src_device, src, size);
+
+			// Perform data transfer using SYCL command group
+			q.submit([&](handler &h)
+					 { h.parallel_for(range<1>(size), [=](id<1> i)
+									  { reinterpret_cast<char *>(dest_device)[i] =
+											reinterpret_cast<char *>(src_device)[i]; }); });
+
+			// Free allocated device memory
+			free(dest_device, q);
+			free(src_device, q);
+		}
+		catch (sycl::exception const &e)
+		{
+			// Handle exceptions, if any
+			std::cerr << "SYCL Exception: " << e.what() << std::endl;
+			return qmckl_failwith_device(context, QMCKL_FAILURE_DEVICE,
+										 "qmckl_memcpy_D2D", "SYCL exception");
+		}
+	}
+	qmckl_unlock_device(context);
+
+	return QMCKL_SUCCESS_DEVICE;
+}
