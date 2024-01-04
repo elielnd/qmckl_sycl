d2129cd32988478d62ac589229020422d74e9c5e
enditar <eliel-timon.nditar@ens.uvsq.fr>
2024-01-04 12:38:29 +0000

    End of qmckl_distance, start of qmckl_electron

--- include/qmckl_blas.hpp
+++ include/qmckl_blas.hpp
@@ -1,6 +1,7 @@
 #pragma once 
 // This file contains functions prototypes for BLAS related functions
 // (mostly manipulation of the device, matrix and tensor types)
+#include <CL/sycl.hpp>
 
 #include <cassert>
 #include <cmath>
