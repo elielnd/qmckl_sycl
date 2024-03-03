118ec16af6f63952e254d4edd8349464841832ec
marcloic <marcloic81@gmail.com>
2024-03-03 20:53:42 +0000

    WIP

--- src/qmckl_distance.cpp
+++ src/qmckl_distance.cpp
@@ -183,7 +183,7 @@ qmckl_exit_code_device qmckl_distance_devices(const qmckl_context_device context
 
     return QMCKL_SUCCESS_DEVICE;
 }
-
+/*
 qmckl_exit_code_device qmckl_distance_rescaled_device(const qmckl_context_device context, const char transa, const char transb, const int64_t m, const int64_t n, const double *A, const int64_t lda, const double *B, const int64_t ldb, double *const C, const int64_t ldc, const double rescale_factor_kappa, queue &q)
 {
 
@@ -570,3 +570,4 @@ qmckl_exit_code_device qmckl_distance_rescaled_deriv_e_device(qmckl_context_devi
     }
     return info;
 }
+*/
